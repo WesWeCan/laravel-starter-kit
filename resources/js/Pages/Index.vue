704600a2fb53df12002ab8e6187b918cc6d2b37a
WesWeCan <79587028+WesWeCan@users.noreply.github.com>
2025-08-11 12:03:52 +0200

    first commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import BasicLayout from '@/Layouts/BasicLayout.vue';
+
+const count = ref(0);
+
+function increment() {
+    count.value++;
+}
+</script>
+
+<template>
+    <BasicLayout>
+        <section>
+            <h1>Hello World</h1>
+            <button @click="increment">Pressed {{ count }} times</button>
+        </section>
+    </BasicLayout>
+</template>
