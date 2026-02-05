552dd97db41cbb8176c704f17f47e193d90772b7
WesWeCan <79587028+WesWeCan@users.noreply.github.com>
2026-02-05 08:58:30 +0100

    UPdated dependencies, added vue icon and laravel boost

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiHumanGreetingVariant } from '@mdi/js';
 import BasicLayout from '@/Layouts/BasicLayout.vue';
 
 const count = ref(0);
@@ -12,7 +14,7 @@ function increment() {
 <template>
     <BasicLayout>
         <section>
-            <h1>Hello World</h1>
+            <h1><svg-icon type="mdi" :path="mdiHumanGreetingVariant" :size="24"></svg-icon>Hello World</h1>
             <button @click="increment">Pressed {{ count }} times</button>
         </section>
     </BasicLayout>
